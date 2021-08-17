d99136df956229259ad6cfc8649ddd836762e34a
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-08-18 04:19:17 +0530

    add: async wrapper

---
+++ functions/src/middleware/async.ts
@@ -0,0 +1,5 @@
+import { NextFunction, RequestHandler, Response, Request } from "express";
+
+type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+
+export const asyncWrap = (handler: AsyncHandler): RequestHandler => async (req, res, next) => handler(req, res, next).catch(next);
