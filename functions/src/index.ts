0b6c633d9013a33ec73472be3c79fddb71bf8abd
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-09-23 18:42:53 +0530

    feat: notify mess update option

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,71 +23,74 @@ export const api = functions.https.onRequest(main);
 //Order place notif on eater side
 
 export const test = functions.firestore.document("orders/{document}").onCreate((res) => {
+	const payload = {
+		notification: {
+			title: "Welcome",
+			body: "thank for installing our app",
+		},
+	};
 
-    const payload = {
-        notification: {
-            title: "Welcome",
-            body: "thank for installing our app",
-        },
-    };
-
-    admin
-        .messaging()
-        .sendToDevice("eoz7U6qFQZWkzVCqSCR9v9:APA91bH2_a0vJT72hw9-FHIXylB0bUekd7f3PUek8EivJdi98ucZaHE95jOD-nYKgnmxLSOoFY2H6h9qb0HrpZjM1zMtjExl5lT0Lzopl0bv4nklGpRdh9MHQzajKf8lFPMj9Oi8RZKp", payload)
-        .then(function (response) {
-            console.log("Notification sent successfully:", response);
-        })
-        .catch(function (error) {
-            console.log("Notification sent failed:", error);
-        });
-})
+	admin
+		.messaging()
+		.sendToDevice(
+			"eoz7U6qFQZWkzVCqSCR9v9:APA91bH2_a0vJT72hw9-FHIXylB0bUekd7f3PUek8EivJdi98ucZaHE95jOD-nYKgnmxLSOoFY2H6h9qb0HrpZjM1zMtjExl5lT0Lzopl0bv4nklGpRdh9MHQzajKf8lFPMj9Oi8RZKp",
+			payload
+		)
+		.then(function (response) {
+			console.log("Notification sent successfully:", response);
+		})
+		.catch(function (error) {
+			console.log("Notification sent failed:", error);
+		});
+});
 
 //Update order status notif on user side
 
 export const updateOrder = functions.firestore.document("orders/{document}").onUpdate((res) => {
+	const payload = {
+		notification: {
+			color: "#f28d0a",
+			icon: "https://www.mujhub.com/static/media/logoSq256.7b56d951.png",
+			title: "Order Status Updated",
+			body: `Order Status : ${res.after.data().status}`,
+		},
+	};
 
-    const payload = {
-        notification: {
-            color: "#f28d0a",
-            icon: "https://www.mujhub.com/static/media/logoSq256.7b56d951.png",
-            title: "Order Status Updated",
-            body: `Order Status : ${res.after.data().status}`,
-        },
-    };
-
-    if (res.after.data().status === "ACCEPTED") {
-        admin
-            .messaging()
-            .sendToDevice(res.after.data().token, payload)
-            .then(function (response) {
-                console.log("Notification sent successfully:", response);
-            })
-            .catch(function (error) {
-                console.log("Notification sent failed:", error);
-            });
-    }
-})
+	if (res.after.data().status === "ACCEPTED") {
+		admin
+			.messaging()
+			.sendToDevice(res.after.data().token, payload)
+			.then(function (response) {
+				console.log("Notification sent successfully:", response);
+			})
+			.catch(function (error) {
+				console.log("Notification sent failed:", error);
+			});
+	}
+});
 
 //Mess Menu Updation
 
-export const messMenuUpdate = functions.firestore.document("mess/menuData").onUpdate((res) => {
+export const messMenuUpdate = functions.firestore.document("mess/menuData").onUpdate((snapshot) => {
+	let after = snapshot.after.data();
+	if (!after.notify) return;
 
-    const payload = {
-        notification: {
-            color: "#f28d0a",
-            title: "Mess Menu Updated",
-            body: "Updated",
-            icon: "https://www.mujhub.com/static/media/logoSq256.7b56d951.png",
-        }
-    }
+	const payload = {
+		notification: {
+			color: "#f28d0a",
+			title: "Mess Menu Updated",
+			body: "Updated",
+			icon: "https://www.mujhub.com/static/media/logoSq256.7b56d951.png",
+		},
+	};
 
-    admin
-        .messaging()
-        .sendToTopic("messMenuUpdate", payload)
-        .then(function (response) {
-            console.log("Notification sent successfully:", response);
-        })
-        .catch(function (error) {
-            console.log("Notification sent failed:", error);
-        });
-})
+	admin
+		.messaging()
+		.sendToTopic("messMenuUpdate", payload)
+		.then(function (response) {
+			console.log("Notification sent successfully:", response);
+		})
+		.catch(function (error) {
+			console.log("Notification sent failed:", error);
+		});
+});
